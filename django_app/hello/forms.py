4b7512a0fa45e6237e49f109a9fab7f4e2e12b28
Mona Lisa <tes@example.co.jp>
2020-10-21 15:11:14 +0900

    [fix]p315まで作成済み

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from.models import Friend, Message
 
 class HelloForm(forms.Form):
     id = forms.IntegerField(label='ID')
@@ -8,3 +8,19 @@ class HelloForm(forms.Form):
 class FindForm(forms.Form):
     find = forms.CharField(label='Find', required=False, widget=forms.TextInput(
         attrs={'class': 'form-control'}))
+
+#P191 FriendForm書いてなかったかも
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name','mail','gender','age','birthday']
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['title','content','friend']
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control form-control-sm'}),
+            'content': forms.Textarea(attrs={'class':'form-control form-control-sm', 'rows':2}),
+            'friend': forms.Select(attrs={'class':'form-control form-control-sm'}),
+        }
