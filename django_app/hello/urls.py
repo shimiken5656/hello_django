e4c8e3ba67abe779086fd4c47ed380da077e427b
Mona Lisa <tes@example.co.jp>
2020-10-24 23:43:19 +0900

    [finish]edit_user

--- django_app/hello/urls.py
+++ django_app/hello/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create',views.create,name='create'),
+    path('create', views.create, name='create'),
+    path('edit/<int:num>',views.edit,name='edit'),
     path('<int:num>', views.index, name='index'),
     path('next2', views.index2, name='index2'),
     path('find', views.find, name='find'),
